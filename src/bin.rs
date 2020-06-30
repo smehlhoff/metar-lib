f41138f7b0a68e17f7f16277cbe795f638d05aab
smehlhoff <stevemehlhoff@gmail.com>
2020-06-29 23:49:14 -0700

    Basic setup to read user arguments

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,13 @@
 use metar_cli::*;
+use std::env;
 
 fn main() {
-    println!("{:#?}", Metar::parse("KSJC"));
+    let args: Vec<String> = env::args().collect();
+    let code = &args[1];
+
+    if code.len() != 4 {
+        panic!("Please enter a valid METAR code.")
+    };
+
+    println!("{:#?}", Metar::parse(code));
 }
