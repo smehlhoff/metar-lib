4d7e87cd010c204921c1cf7306c454c52bcd6d78
smehlhoff <stevemehlhoff@gmail.com>
2020-07-17 03:19:52 -0700

    Change variable name

--- examples/simple_cli.rs
+++ examples/simple_cli.rs
@@ -3,7 +3,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let code = &args[1];
+    let metar = &args[1];
 
-    println!("{:#?}", Metar::parse(code));
+    println!("{:#?}", Metar::parse(metar));
 }
